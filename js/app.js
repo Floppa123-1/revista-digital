98b5c1a2a7a930f492102a7905b1dfe8273100e2
Floppa123-1 <felipecampos0415@gmail.com>
2026-06-12 20:07:57 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 const container = document.getElementById("book");
 
-const bookWidth = Math.min(window.innerWidth * 0.9, 900);
-const bookHeight = bookWidth * 1.4;
+const isMobile = window.innerWidth < 768;
+
+const bookWidth = isMobile
+    ? window.innerWidth * 0.95
+    : Math.min(850, window.innerWidth * 0.8);
+
+const bookHeight = isMobile
+    ? bookWidth * 1.5
+    : bookWidth * 1.3;
 
 const pageFlip = new St.PageFlip(container, {
     width: bookWidth,
     height: bookHeight,
     showCover: true,
+    size: "stretch",
     mobileScrollSupport: true
 });
 
