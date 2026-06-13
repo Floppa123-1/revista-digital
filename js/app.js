2ac3db70a3d7d8deb751a12751700316e7ec7677
Floppa123-1 <felipecampos0415@gmail.com>
2026-06-12 20:02:36 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
-const pageFlip = new St.PageFlip(
-    document.getElementById("book"),
-    {
-        width: 600,
-        height: 800,
-        showCover: true
-    }
-);
+const container = document.getElementById("book");
+
+const bookWidth = Math.min(window.innerWidth * 0.9, 900);
+const bookHeight = bookWidth * 1.4;
+
+const pageFlip = new St.PageFlip(container, {
+    width: bookWidth,
+    height: bookHeight,
+    showCover: true,
+    mobileScrollSupport: true
+});
 
 pageFlip.loadFromHTML(
     document.querySelectorAll(".page")
